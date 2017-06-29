007ec39ce18c0e28f369025c4b1e322c0f5ec85c
felixottander <felix.ottander@yahoo.com>
2017-06-29 15:37:28 +0200

    Restores json response helper and adds rspec tests for the file attachmnet.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,8 +14,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryGirl::Syntax::Methods
-  config.include ResponseJSON, type: :request
   config.include Paperclip::Shoulda::Matchers
+  config.include ResponseJSON, type: :request
 end
 
 Shoulda::Matchers.configure do |config|
