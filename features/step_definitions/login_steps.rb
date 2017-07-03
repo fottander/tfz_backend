d4604abfc0bb65098ad8da70b5fd9611109b5d4f
felixottander <felix.ottander@yahoo.com>
2017-07-03 21:40:42 +0200

    Adds devise gem and some basic configurations. Adds step defs.

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,10 +1,11 @@
 Given(/^the following users exist$/) do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+  table.hashes.each do |hash|
+    create(:user, hash)
+  end
 end
 
 Given(/^I am on the root page$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit root_path
 end
 
 Given(/^I select "([^"]*)" from "([^"]*)"$/) do |arg1, arg2|
