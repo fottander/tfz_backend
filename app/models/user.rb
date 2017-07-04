cae518ec63eae1999f8c3c370d0cf7c475db48b1
felixottander <felix.ottander@yahoo.com>
2017-07-04 12:03:11 +0200

    Removes the ability to register as an outside visitor.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   VALID_ROLES = ['admin', 'superadmin']
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :role, inclusion: {in: VALID_ROLES, message: '%{value} is not a valid user role'}
