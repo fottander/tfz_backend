1d788a2b9528725dd304115d621ca7ad2aef1de9
felixottander <felix.ottander@yahoo.com>
2017-07-05 19:05:54 +0200

    Fixes the sign in error for request test.

--- spec/requests/api/v1/news.rb
+++ spec/requests/api/v1/news.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 RSpec.describe Api::V1::NewsController, type: :request do
   let!(:news) { create(:news, title: 'My trip to Aspen', content: 'Aspen is great') }
 
+  describe 'sessions' do
+    it 'signs in user' do
+      user = User.create!(email: "fuck@tfz.com", password: "very-secret")
+      sign_in user
+    end
+  end
+
   describe 'GET /v1/news' do
     it 'should return all the news titles and contents' do
       get '/api/v1/news'
