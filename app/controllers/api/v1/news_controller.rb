5c54542d9e8647d9337eb4d3a881132116dc3ab1
fottander <felix.ottander@yahoo.com>
2017-07-11 10:54:16 +0200

    Adds show action for api news endpoint and modifies the request tests for nnews endpoint. (#21)

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -2,4 +2,12 @@ class Api::V1::NewsController < ApiController
   def index
     @news = News.all
   end
+
+  def show
+    begin
+      @news = News.find(params[:id])
+    rescue => error
+      render json: { message: error}, status: 404
+    end
+  end
 end
--- spec/requests/api/v1/news.rb
+++ spec/requests/api/v1/news.rb
@@ -22,4 +22,24 @@ RSpec.describe Api::V1::NewsController, type: :request do
       expect(response.status).to eq 200
     end
   end
+
+  describe 'GET /v1/news' do
+    it 'should return the news title,content and file' do
+      get "/api/v1/news/#{news.id}"
+
+      expected_response = {
+        'id' => news.id, 'title' => 'My trip to Aspen', 'content' => 'Aspen is great'
+      }
+
+      expect(response_json).to eq expected_response
+      expect(response.status).to eq 200
+    end
+
+    it 'should render error message on failure' do
+      get '/api/v1/news/999999'
+      expected_response = {message: "Couldn't find News with 'id'=999999"}
+      expect(response_json).to eq JSON.parse(expected_response.to_json)
+      expect(response.status).to eq 404
+    end
+  end
 end
