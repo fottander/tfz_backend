3c88f83ffc0bd965af93bded6a30df9acbb86718
felixottander <felix.ottander@yahoo.com>
2017-08-17 14:07:51 +0200

    Adds serach by tagging, and showing tags by created at descending order. Also changing destroy command to work with foreign key.

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,6 +1,6 @@
 class News < ApplicationRecord
   has_many :taggings
-  has_many :tags, through: :taggings
+  has_many :tags, through: :taggings, dependent: :destroy
 
   def all_tags=(names)
     self.tags = names.split(",").map do |name|
