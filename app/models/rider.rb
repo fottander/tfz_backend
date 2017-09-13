daf190fac979047c61ee7d6a0ff78bcf42a2f78d
felixottander <felix.ottander@yahoo.com>
2017-09-13 16:05:33 +0200

    Adds search functinality in rider page and refactors the model and controller.

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,3 +1,11 @@
 class Rider < ApplicationRecord
   validates_presence_of :name
+
+  def self.search(term)
+    if term
+      where('name ILIKE ?', "%#{term}%").order(name: :asc)
+    else
+      all.order(name: :asc)
+    end
+  end
 end
